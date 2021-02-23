c368b5ddcb68d5ac56f62434bbfdfb02b01fecce
kimjunhyeok <kimjunhyuk1231@gmail.com>
2021-02-23 23:59:19 +0900

    Creating Sign In Component.

--- src/pages/signin.jsx
+++ src/pages/signin.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Signin from '../components/Signin';
 
 const SigninPage = () => {
     return (
         <div>
-            <h1>Signin Page.</h1>
+            <Signin />
         </div>
     )
 }
